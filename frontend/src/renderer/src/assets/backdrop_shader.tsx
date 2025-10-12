c03058ed6ffaf6a5d0827a66bae9ed0a5dd10237
JulianKonowalski <julian.konwalski@gmail.com>
2025-10-12 21:55:58 +0200

    add text edit shader backdrop

---
+++ frontend/src/renderer/src/assets/backdrop_shader.tsx
@@ -0,0 +1,45 @@
+// yes, glsl fragment shader in ts source, fight me
+export const backdrop_shader =  `
+#ifdef GL_ES
+  precision lowp float;
+#endif
+
+uniform vec2 u_mouse;
+uniform vec2 u_resolution;
+uniform float u_cursor_pos;
+
+const vec3 black  = vec3(0.086, 0.086, 0.094);
+
+const vec3 base_blue   = vec3(0.008, 0.455, 0.722) * 0.4;
+const vec3 base_purple = vec3(0.427, 0.090, 0.600) * 0.6;
+
+const vec2 base_blue_pos    = vec2(1.2, 0.6);
+const vec2 base_purple_pos  = vec2(-0.7, -0.3);
+
+const float radius = 1.5;
+
+void main() {
+
+  float sin_cursor_pos = sin(u_cursor_pos * 0.01);
+  float cos_cursor_pos = cos(u_cursor_pos * 0.01);
+
+  vec3 blue   = vec3(base_blue.x - cos_cursor_pos * 0.01, base_blue.y, base_blue.z + sin_cursor_pos * 0.1);
+  vec3 purple = vec3(base_purple.x + cos_cursor_pos * 0.01, base_purple.y, base_purple.z - sin_cursor_pos * 0.1);
+
+  vec2 st = gl_FragCoord.xy / u_resolution;
+  vec2 mouse_modifier = (u_mouse / u_resolution) * 2.0 - vec2(1.0, 1.0);
+
+  vec2 blue_pos   = base_blue_pos - (mouse_modifier * 0.01) + (vec2(cos_cursor_pos, sin_cursor_pos) * 0.01);
+  vec2 purple_pos = base_purple_pos - (mouse_modifier * 0.03) - (vec2(cos_cursor_pos, sin_cursor_pos) * 0.01);
+
+  float blue_alpha    = 1.0 - distance(st, blue_pos) / radius;
+  blue_alpha    = blue_alpha > 0.0 ? blue_alpha : 0.0;
+  
+  float purple_alpha  = 1.0 - distance(st, purple_pos) / (radius * 1.2);
+  purple_alpha  = purple_alpha > 0.0 ? purple_alpha : 0.0;
+
+  vec3 mixed_color = purple * purple_alpha + blue * blue_alpha;
+  vec3 final_color = vec3(max(mixed_color.x, black.x), max(mixed_color.y, black.y), max(mixed_color.z, black.z));
+
+  gl_FragColor = vec4(final_color, 1.0);
+}`;
--- frontend/src/renderer/src/components/TextEdit.tsx
+++ frontend/src/renderer/src/components/TextEdit.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import GlslCanvas from "glslCanvas";
+import { backdrop_shader } from "@renderer/assets/backdrop_shader";
 
 export default function TextEdit(): React.JSX.Element {
-  return <div className="text-field" contentEditable="plaintext-only"/>;
+  const canvas_ref  = useRef<HTMLCanvasElement | null>(null);
+  const edit_ref    = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (canvas_ref.current === null || edit_ref.current === null) { return; }
+    const sandbox: GlslCanvas = new GlslCanvas(canvas_ref.current);
+    sandbox.load(backdrop_shader);
+
+    edit_ref.current.addEventListener("input", () => {
+      sandbox.setUniform("u_cursor_pos", document.getSelection()?.getRangeAt(0).startOffset);
+    });
+  }, []);
+
+  return (
+    <>
+      <canvas ref={canvas_ref} className="glslCanvas"/>
+      <div ref={edit_ref} className="text-field" contentEditable="plaintext-only"/>;
+    </>
+  );
 }
