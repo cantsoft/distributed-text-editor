e83eaff9e9f5da45d543ff5ada79ac11c0baa684
JulianKonowalski <julian.konwalski@gmail.com>
2025-10-13 20:50:15 +0200

    update backdrop shader

--- frontend/src/renderer/src/assets/backdrop_shader.tsx
+++ frontend/src/renderer/src/assets/backdrop_shader.tsx
@@ -6,12 +6,10 @@ export const backdrop_shader =  `
 
 uniform vec2 u_mouse;
 uniform vec2 u_resolution;
-uniform float u_cursor_pos;
 
 const vec3 black  = vec3(0.086, 0.086, 0.094);
-
-const vec3 base_blue   = vec3(0.008, 0.455, 0.722) * 0.4;
-const vec3 base_purple = vec3(0.427, 0.090, 0.600) * 0.6;
+const vec3 blue   = vec3(0.008, 0.455, 0.722) * 0.4;
+const vec3 purple = vec3(0.427, 0.090, 0.600) * 0.6;
 
 const vec2 base_blue_pos    = vec2(1.2, 0.6);
 const vec2 base_purple_pos  = vec2(-0.7, -0.3);
@@ -19,18 +17,11 @@ const vec2 base_purple_pos  = vec2(-0.7, -0.3);
 const float radius = 1.5;
 
 void main() {
-
-  float sin_cursor_pos = sin(u_cursor_pos * 0.01);
-  float cos_cursor_pos = cos(u_cursor_pos * 0.01);
-
-  vec3 blue   = vec3(base_blue.x - cos_cursor_pos * 0.01, base_blue.y, base_blue.z + sin_cursor_pos * 0.1);
-  vec3 purple = vec3(base_purple.x + cos_cursor_pos * 0.01, base_purple.y, base_purple.z - sin_cursor_pos * 0.1);
-
   vec2 st = gl_FragCoord.xy / u_resolution;
   vec2 mouse_modifier = (u_mouse / u_resolution) * 2.0 - vec2(1.0, 1.0);
 
-  vec2 blue_pos   = base_blue_pos - (mouse_modifier * 0.01) + (vec2(cos_cursor_pos, sin_cursor_pos) * 0.01);
-  vec2 purple_pos = base_purple_pos - (mouse_modifier * 0.03) - (vec2(cos_cursor_pos, sin_cursor_pos) * 0.01);
+  vec2 blue_pos   = base_blue_pos - (mouse_modifier * 0.01);
+  vec2 purple_pos = base_purple_pos - (mouse_modifier * 0.03);
 
   float blue_alpha    = 1.0 - distance(st, blue_pos) / radius;
   blue_alpha    = blue_alpha > 0.0 ? blue_alpha : 0.0;
