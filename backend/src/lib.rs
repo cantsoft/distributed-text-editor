f9a3c51e03aee1a6989d07174567a34eb9dd2dfd
Jan <jan@zadrag.net>
2025-11-09 22:53:12 +0100

    created dataset_test

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -8,13 +8,23 @@ pub use doc::Doc;
 use node_crdt::Position;
 use side::Side;
 use tree_crdt::TreeCRDT;
+use serde::Deserialize;
 
 // use napi_derive::napi;
 // use tokio::time::sleep;
+#[derive(Debug, Deserialize)]
+pub struct OperationData {
+    pub char: char, 
+    pub position: u32,
+    pub id: u32,
+    pub user_id: u8,
+    pub timestamp: u64,
+    pub type_of_operation: char // 'i' = insert, 'd' = delete
+}
 
 #[cfg(test)]
 mod tests {
-
+    use std::{fs, sync::Arc};
     use super::*;
 
     #[test]
@@ -53,4 +63,31 @@ mod tests {
         println!("{}", doc_str);
         assert_eq!(doc_str, "");
     }
+
+    #[test]
+    pub fn dataset_test() {
+        let data = fs::read_to_string("../data/test_dataset_adding.json").unwrap();
+        let ops: Vec<OperationData> = serde_json::from_str(&data.as_str()).unwrap();
+        let mut doc = Doc::new();
+            for op in ops {
+                let pos: Arc<[Position]> = Arc::from([Position {
+                    digit: op.position,
+                    peer_id: op.user_id,
+                    time: op.timestamp,
+                }]);
+                if op.type_of_operation == 'i' {
+                    doc.tree_mut().insert(&pos, op.char);
+                } else if op.type_of_operation == 'd' {
+                    doc.tree_mut().remove(&pos);
+                }
+            }
+        let text = doc.tree().collect_string();
+        let ground = std::fs::read_to_string("../data/test_dataset_adding.json_ground_truth.txt").unwrap();
+        let ground = ground.replace("\r\n", "\n");
+
+        println!("Reconstructed text: {}", text);
+        assert_eq!(text, ground, "\nReconstructed text does not match ground truth");
+
+    }
+
 }
