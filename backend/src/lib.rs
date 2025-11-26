79eddfcedf7c0a09ea68e52f4a8c875c90aa912f
Ezic04 <slawek.tomasz.brzozka@gmail.com>
2025-11-26 17:31:20 +0100

    stdio ipc prototype

--- backend/src/lib.rs
+++
@@ -1,7 +0,0 @@
-mod doc;
-mod side;
-#[cfg(test)]
-mod tests;
-mod types;
-
-pub use doc::Doc;
---
+++ backend/src/main.rs
@@ -0,0 +1,17 @@
+mod doc;
+mod side;
+#[cfg(test)]
+mod tests;
+mod types;
+
+pub use doc::Doc;
+
+use std::io::{Write, stdin, stdout};
+
+fn main() {
+    let _ = stdout().write("hello form backend\n".as_bytes());
+    let mut str_buf = String::new();
+    let size_or_err = stdin().read_line(&mut str_buf);
+    println!("{:?}", size_or_err);
+    println!("{}", str_buf);
+}
