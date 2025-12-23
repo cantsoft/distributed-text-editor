dab03a6e48bd82a35f69effc2db40eb8144ee962
Ezic04 <slawek.tomasz.brzozka@gmail.com>
2025-12-23 15:19:58 +0100

    add readFromBackend function

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,23 +6,41 @@
 
 // pub use doc::Doc;
 
-// use std::io::{Write, stdin, stdout};
-
-// fn main() {
-//     let _ = stdout().write("hello form backend\n".as_bytes());
-//     let mut str_buf = String::new();
-//     let size_or_err = stdin().read_line(&mut str_buf);
-
-//     println!("{:?}", size_or_err);
-//     println!("{}", str_buf);
-// }
-
 use prost::Message;
-
+use std::io::{self, Read, Write, stdin, stdout};
 pub mod proto {
     include!(concat!(env!("OUT_DIR"), "/dte.rs"));
 }
+use crate::proto::{UserInsert, UserOperation, user_operation};
 
-use proto::UserOperation;
+fn main() -> io::Result<()> {
+    let usr_op = UserOperation {
+        position: 2,
+        operation_type: Some(user_operation::OperationType::Insert(UserInsert {
+            char: 97,
+        })),
+    };
+    let binary = usr_op.encode_to_vec();
+    let length = binary.len() as u32;
+    let mut stdout = io::stdout().lock();
+    stdout.write_all(&length.to_be_bytes()).expect("write len");
+    stdout.write_all(&binary).expect("write body");
+    stdout.flush().expect("flush");
 
-fn main() {}
+    let mut stdin = io::stdin().lock();
+    loop {
+        let mut len_buf = [0u8; 4];
+        if let Err(_) = stdin.read_exact(&mut len_buf) {
+            break;
+        }
+        let len = u32::from_be_bytes(len_buf) as usize;
+
+        let mut payload_buf = vec![0u8; len];
+        stdin.read_exact(&mut payload_buf)?;
+
+        let op = UserOperation::decode(&payload_buf[..])?;
+        println!("Received op at pos: {}", op.position);
+    }
+
+    Ok(())
+}
