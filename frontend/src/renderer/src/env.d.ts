f7de00cda0e2fef3d1cc6bbba679a60a116e40aa
JulianKonowalski <julian.konwalski@gmail.com>
2026-01-04 20:19:33 +0100

    rust-js communication

--- frontend/src/renderer/src/env.d.ts
+++ frontend/src/renderer/src/env.d.ts
@@ -1,15 +1,12 @@
 /// <reference types="vite/client" />
 
-import { Doc } from "../../../native";
-type DocAPI = Omit<Doc, "constructor">;
-
 declare global {
   interface Window {
     api: {
       minimize: () => void;
       maximize: () => void;
       close: () => void;
-      createDocument: () => DocAPI;
+      onUserKeydown(key: string, cursor_pos: number | undefined): unknown;
     };
   }
 }
